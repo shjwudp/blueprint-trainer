27db582d1a0a65ee0e8f7de9f118b1c31b5e7e38
Jianbin Chang <shjwudp@gmail.com>
2023-07-18 17:08:35 +0800

    feat: add gpu indicators

---
+++ blueprint_trainer/system/__init__.py
@@ -0,0 +1,10 @@
+from wandb.sdk.internal.system.assets.gpu import (
+    pynvml,
+    GPUMemoryUtilization,
+    GPUMemoryAllocated,
+    GPUMemoryAllocatedBytes,
+    GPUUtilization,
+    GPUTemperature,
+    GPUPowerUsageWatts,
+    GPUPowerUsagePercent,
+)
